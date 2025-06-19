892d4496987d2b94944537988c594b74108c321a
Somiyana <somiyanamir@gmail.com>
2025-06-19 15:35:24 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'HP LaserJet Printer'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
